5f25190e2cb20e91799810e7d0e930b94b934d9f
gitgitWi <wiii@kakao.com>
2022-12-06 11:29:19 +0000

    feat(layout) index 페이지에 DefaultLayout 컴포넌트 적용

--- packages/apps/web/src/pages/_app.tsx
+++ packages/apps/web/src/pages/_app.tsx
@@ -2,9 +2,8 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
-import { trpc } from "../utils/trpc";
-
-import "../styles/globals.css";
+import { trpc } from "@/utils/trpc";
+import "@/styles/globals.scss";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
--- packages/apps/web/src/pages/index.tsx
+++ packages/apps/web/src/pages/index.tsx
@@ -1,20 +1,17 @@
 import { type NextPage } from "next";
 
-import { NextHead } from "@features/next-head";
+import { DefaultLayout } from "@features/ui/layouts";
 import { trpc } from "@/utils/trpc";
 
 const Home: NextPage = () => {
   const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
 
   return (
-    <>
-      <NextHead />
-      <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
-        <div className="flex w-full items-center justify-center pt-6 text-2xl text-blue-500">
-          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
-        </div>
-      </main>
-    </>
+    <DefaultLayout>
+      <div className="flex w-full items-center justify-center text-2xl">
+        {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
+      </div>
+    </DefaultLayout>
   );
 };
 
